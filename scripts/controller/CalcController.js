998bed0750123d322a8cf84cbac9075bed05eda0
Kleiton Viana das Neves <kleiton.neves@estudante.ifb.edu.br>
2022-02-11 21:19:52 -0300

    Calculadora

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,41 @@
+class CalcController {
+    
+    constructor(){
+        
+        this._displayCalc = "0"; 
+        this._currentDate;
+        this.initialize;
+    }
+
+    initialize(){
+        
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+
+    }
+
+    get displayCalc(){
+        
+        return this._displayCalc;
+
+    }
+    set displayCalc(valor){
+
+        this._displayCalc = valor; 
+    }
+
+    get currentDate(){
+
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+
+        return this._currentDate = valor;
+    }
+
+
+}
